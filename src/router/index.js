cf5235ec318f5d5c6f1aa232bfdd53be586c5123
adit.it <adit.it@langitpayment.com>
2023-07-23 16:40:00 +0700

    first init

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+
+const routes = [
+  {
+    path: "/",
+    name: "MainPage",
+    // component: MainPage
+  },
+  {
+    path: "/my-cart",
+    name: "MyCart",
+    // component: MyCart
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/'
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  base: "/",
+  routes,
+  linkActiveClass: "active",
+  linkExactActiveClass: "active",
+  scrollBehavior(savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return {
+        selector: window.scrollTo(0, 0),
+        behavior: "smooth",
+      };
+    }
+  },
+});
+
+export default router;
